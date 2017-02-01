6a4128170e95b8f61f6d673ca243645d00ab8780
Christianmichael Dutton <christianmichael_dutton@yahoo.com>
2017-01-31 20:26:58 -0800

    Updated Assigned files for js and jquery

--- WebFundamentals/Javascript/Students.js
+++ WebFundamentals/Javascript/Students.js
@@ -1,13 +1,12 @@
-function studentsandinstructors(){
-  var users = {
- 'Students': [
+function students(){
+  var students = [
      {first_name:  'Michael', last_name : 'Jordan'},
      {first_name : 'John', last_name : 'Rosales'},
      {first_name : 'Mark', last_name : 'Guillen'},
      {first_name : 'KB', last_name : 'Tonel'}
-  ],
- 'Instructors': [
-     {first_name : 'Michael', last_name : 'Choi'},
-     {first_name : 'Martin', last_name : 'Puryear'}
-  ]
- }
+]
+  for (var i = 0; i < students.length; i++){
+    console.log(students[i].first_name +" "+ students[i].last_name);
+  }
+}
+students()
