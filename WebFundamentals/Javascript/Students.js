cbc457648d838d09faa518754545bf0ba7560789
Christianmichael Dutton <christianmichael_dutton@yahoo.com>
2017-02-03 11:56:14 -0800

    Added Javascript assignments

--- WebFundamentals/Javascript/Students.js
+++ WebFundamentals/Javascript/Students.js
@@ -5,8 +5,9 @@ function students(){
      {first_name : 'Mark', last_name : 'Guillen'},
      {first_name : 'KB', last_name : 'Tonel'}
 ]
-  for (var i = 0; i < students.length; i++){
+for (var i = 0; i < students.length; i++){
     console.log(students[i].first_name +" "+ students[i].last_name);
   }
+  console.log(students[0].last_name);
 }
 students()
--- WebFundamentals/Javascript/studentsandinstructors.js
+++ WebFundamentals/Javascript/studentsandinstructors.js
@@ -10,9 +10,12 @@ function studentsandinstructors(){
        {first_name : 'Michael', last_name : 'Choi'},
        {first_name : 'Martin', last_name : 'Puryear'}
     ]
-   }
-   for (var i = 0; i<users.length; i++){
-     console.log(i);
-   }
+  }
+  for (var i = 0; i < users.Students.length; i++){
+      console.log((i+1)+" - "+users.Students[i].first_name+" "+users.Students[i].last_name+" - "+(users.Students[i].first_name.length+users.Students[i].last_name.length));
+    }
+    for (var i = 0; i < users.Instructors.length; i++){
+        console.log((i+1)+" - "+users.Instructors[i].first_name+" "+users.Instructors[i].last_name+" - "+(users.Instructors[i].first_name.length+users.Instructors[i].last_name.length));
+    }
 }
 studentsandinstructors()
